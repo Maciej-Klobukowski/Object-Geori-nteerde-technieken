d16997ef88169840fb41e00c8ef529ff22ae745d
Maciej-Klobukowski <12401667@student.pxl.be>
2025-11-16 23:35:33 +0100

    Add MainWindow class header file
    Defined the MainWindow class. Added UI elements (buttons and label) and
    composed Circle and Rectangle objects. Prepares signal/slot connections
    for drawing actions.
    git add README.md circle.h mainwindow.h CMakeLists.txt LICENSE.txt make.list

--- mainwindow.h
+++ mainwindow.h
@@ -9,11 +9,23 @@
 #include "rectangle.h"
 #include "drawingtool.h"
 
+<<<<<<< HEAD
+=======
+/*
+ * CLASS: MainWindow
+ * This class uses composition to include UI elements and Shape objects.
+ * It demonstrates correct object-oriented structuring of a UI controller class.
+ */
+>>>>>>> 6a14b95 (Add MainWindow class header file)
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+<<<<<<< HEAD
     explicit MainWindow(QWidget *parent = nullptr);
+=======
+    MainWindow(QWidget *parent = nullptr);
+>>>>>>> 6a14b95 (Add MainWindow class header file)
 
 private slots:
     void drawCircle();
@@ -21,6 +33,15 @@ private slots:
 
 private:
     QLabel* label;
+<<<<<<< HEAD
+=======
+
+    /*
+     * OBJECT COMPOSITION:
+     * The window HAS-A Circle and Rectangle.
+     * This is correct because a window is not a shape, but uses them.
+     */
+>>>>>>> 6a14b95 (Add MainWindow class header file)
     Circle circle;
     Rectangle rect;
 };
