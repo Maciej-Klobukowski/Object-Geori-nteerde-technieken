92299c84aefb96b5791145e1da453072d0f47926
Maciej_klobukowski <12401667@student.pxl.be>
2025-11-29 15:20:47 +0100

    changed make.list
    CC

---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ObjectGeorienteerdeTechnieken_FILE C:/Users/Maciej/Documents/Object-Geori-nteerde-technieken/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ObjectGeorienteerdeTechnieken.exe)
+set(__QT_DEPLOY_TARGET_ObjectGeorienteerdeTechnieken_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ObjectGeorienteerdeTechnieken_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
